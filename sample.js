a4443a1bad9b4e79be154629a5a5d988285081ed
Rachelle Yeo <rachelle.yeo@outlook.com>
2023-06-22 13:45:57 +0800

    update as one function for multiple clips

--- sample.js
+++ sample.js
@@ -1,45 +1,56 @@
-const ctx = new AudioContext();
+function initAudioPlayer(audioURL) {
+  const ctx = new AudioContext();
 
-let audio;
-let playSound;
-let audioTitle;
+  let audio;
+  let playSound;
+  let audioTitle;
 
-const body = document.body;
+  const container = document.createElement('div');
+  const body = document.body;
+  body.appendChild(container);
 
-fetch('audio/synth-organ.ogg')
-  .then(response => {
-    const url = response.url.split('/');
-    audioTitle = url.at(-1).toString()
-    const audioTitleText = document.createElement('p');
-    audioTitleText.textContent = audioTitle;
-    body.appendChild(audioTitleText);
-    return response.arrayBuffer()
-  })
-  .then(arrayBuffer => ctx.decodeAudioData(arrayBuffer))
-  .then(decodedAudio => {
-    audio = decodedAudio;
-  });
+  fetch(audioURL)
+    .then(response => {
+      const url = response.url.split('/');
+      audioTitle = url.at(-1).toString();
+      const audioTitleText = document.createElement('p');
+      audioTitleText.textContent = audioTitle;
+      container.appendChild(audioTitleText);
+      return response.arrayBuffer();
+    })
+    .then(arrayBuffer => ctx.decodeAudioData(arrayBuffer))
+    .then(decodedAudio => {
+      audio = decodedAudio;
+    });
 
-function playBack() {
-  playSound = ctx.createBufferSource();
-  playSound.buffer = audio;
-  playSound.connect(ctx.destination);
-  playSound.start(0);
-}
-
-function stopPlayback() {
-  if (playSound) {
-    playSound.stop();
+  function playBack() {
+    playSound = ctx.createBufferSource();
+    playSound.buffer = audio;
+    playSound.connect(ctx.destination);
+    playSound.addEventListener('ended', () => {
+      playButton.disabled = false;
+    });
+    playSound.start(0);
+    playButton.disabled = true;
   }
+
+  function stopPlayback() {
+    if (playSound) {
+      playSound.stop();
+    }
+  }
+
+  const playButton = document.createElement('button');
+  playButton.textContent = 'Play';
+  playButton.addEventListener('click', playBack);
+
+  const stopButton = document.createElement('button');
+  stopButton.textContent = 'Stop';
+  stopButton.addEventListener('click', stopPlayback);
+
+  container.appendChild(playButton);
+  container.appendChild(stopButton);
 }
 
-const playButton = document.createElement('button');
-playButton.textContent = 'Play';
-playButton.addEventListener('click', playBack);
-
-const stopButton = document.createElement('button');
-stopButton.textContent = 'Stop';
-stopButton.addEventListener('click', stopPlayback);
-
-body.appendChild(playButton);
-body.appendChild(stopButton);
+initAudioPlayer('audio/synth-organ.ogg');
+initAudioPlayer('audio/new-wave-kit.ogg');
